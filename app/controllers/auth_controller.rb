aeea6e99d7f4c278c0a2c4e32efa841f805a9c2e
Sharonne24 <sawuor395@gmail.com>
2024-02-24 15:59:13 +0300

    controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,2 +1,9 @@
 class AuthController < ApplicationController
+    def login
+        user = User.find_by(username: params[:username])
+    
+        if user && user.authenticate(params[:password])
+          token = encode_token(user_id: user.id)
+          render json: { user: user, token: token }
+        else
 end
